b34b282ed01373b1b83cbc6e1eae57bade43118d
neeraj <nkthecoder@gmail.com>
2020-05-12 09:44:47 +0530

    tupple file

---
+++ tupple.py
@@ -0,0 +1,11 @@
+#creating a tupple be like
+t=(1,2,3,3,4,5,5,6,7,7,8,5)
+#to know the length of a tupple
+print(len(t))
+#slicing and indexing
+print(t[1:3])
+print(t[4])
+#to know how many time a particular value is stored in tupple
+print(t.count(3))
+#to get indexing position of a element on tupple
+print(t.index(4))
