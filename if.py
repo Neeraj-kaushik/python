308384f5cfe82f357eb12ea4f0e4718df7429a4d
neeraj <nkthecoder@gmail.com>
2020-05-12 10:21:24 +0530

    if statement

---
+++ if.py
@@ -0,0 +1,7 @@
+#here is simple program of if statement
+#program to check wheather the no is even or not 
+a=int(input("enter the no "))
+if a%2==0:
+    print("the no is even ")
+else:
+    print("the no is odd")
