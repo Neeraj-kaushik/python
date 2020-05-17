db46e56d739d6e4c6ee8f57d0bf912e9ac63b7f3
neeraj <nkthecoder@gmail.com>
2020-05-17 17:20:32 +0530

    prime no

---
+++ for7.py
@@ -0,0 +1,12 @@
+a=[]
+
+for b in range(1,101):
+    if b > 1:
+        for i in range (2,b):
+            if (b%i)==0:
+                break
+            else:
+                a.append(b)
+    
+print("and the no are",a)
+
