8808ad70b2542e828b484be3437d2fd6503f1d77
neeraj <nkthecoder@gmail.com>
2020-05-19 17:27:35 +0530

    function2.py

---
+++ function2.py
@@ -0,0 +1,7 @@
+def circle(a):
+    circumference=2*3.14*a
+    area=3.14*a*a
+    print("circumference of circle",circumference)
+    print("area of circle",area)
+a=float(input("enter the radius of circle"))
+circle(a)
