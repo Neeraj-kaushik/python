3a83133b78b8ccd687777db00a54f30f68e39f43
neeraj <nkthecoder@gmail.com>
2020-05-19 17:22:43 +0530

    function1.py

---
+++ function1.py
@@ -0,0 +1,8 @@
+def rectangle(a,b):
+    pmeter=2*(a+b)
+    ameter=a*b
+    print("The perimeter of rectangle is",pmeter)
+    print("the area of rectangle is",ameter)
+a=float(input("enter length of rectangle"))
+b=float(input("enter the breadth of rectangle"))
+rectangle(a,b)
